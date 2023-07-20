e46d0d685ead506c784b75a5d867bf2c63bc4752
jingwen.mao <jingwen.mao@mintegral.com>
2023-07-20 10:39:42 +0800

    feat: 添加表达式展示

--- math-expression-runner.user.js
+++ math-expression-runner.user.js
@@ -3,11 +3,12 @@
 // @author       Momo707577045
 // @version      0.0.1
 // @include      *
+// @exclude      http://blog.luckly-mjw.cn/*
 // @exclude      https://blog.luckly-mjw.cn/*
 // @namespace    https://github.com/Momo707577045/math-expression-runner
 // @description  https://github.com/Momo707577045/math-expression-runner 配套插件
-// @downloadURL	 https://github.com/Momo707577045/m3u8-downloader/raw/math-expression-runner.user.js
-// @updateURL	   https://github.com/Momo707577045/m3u8-downloader/raw/math-expression-runner.user.js
+// @downloadURL	 https://blog.luckly-mjw.cn/tool-show/math-expression-runner/math-expression-runner.user.js
+// @updateURL	   https://blog.luckly-mjw.cn/tool-show/math-expression-runner/math-expression-runner.user.js
 // @grant        none
 // @run-at document-start
 // ==/UserScript==
@@ -22,8 +23,8 @@
     const iframe = document.createElement('iframe');
     iframe.src = 'https://blog.luckly-mjw.cn/tool-show/math-expression-runner/index.html';
     iframe.id = 'math-expression-runner';
-    iframe.style.width = '500px';
-    iframe.style.height = '80px';
+    iframe.style.width = '440px';
+    iframe.style.height = '94px';
     iframe.style.border = 'solid 1px #ccc';
     iframe.style.borderRadius = '4px';
     iframe.style.position = 'fixed';
